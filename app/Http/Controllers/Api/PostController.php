eea109e475d8a8cdaebb19bfc83ce86be97f95e0
han-pv <pirliyev17@gmail.com>
2025-11-29 09:53:57 +0500

    2.2.1 - add API route

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,16 +2,34 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Post;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $posts = Post::paginate(10);
+
+        $objs = [];
+        foreach ($posts as $post) {
+            $objs[] = [
+                'id' => $post->id,
+                'user_id' => $post->user_id,
+                'content' => $post->content,
+                'image_path' => $post->image_path,
+                'video_path' => $post->video_path,
+                'view_count' => $post->view_count,
+                'created_at' => $post->created_at,
+                'updated_at' => $post->updated_at
+            ];
+        }
         return response()->json([
-            ['id' => 1, 'title' => 'First Post', 'content' => 'This is the content of the first post.'],
-            ['id' => 2, 'title' => 'Second Post', 'content' => 'This is the content of the second post.'],
-        ]);
+            'status' => 1,
+            'data' => $objs,
+            'current_page' => $posts->currentPage(),
+            'per_page' => $posts->perPage(),
+            'total' => $posts->total(),
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,11 @@
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [PostController::class, 'index']);
+// API Routes
+// !!! Barde hokman auth goymaly. Hazirlikce acyk api !!!
+// (Auth:sanctum) goymaly
+
+// api/v1/posts
+Route::prefix('v1')->group(function () {
+    Route::get('/posts', [PostController::class, 'index']);
+});
